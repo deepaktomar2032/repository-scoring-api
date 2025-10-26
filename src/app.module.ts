f6d5b3d8d92244956834f89ac8407744afb28546
deepaktomar2032 <deepaktomar2032@gmail.com>
2025-10-27 00:23:41 +0530

    feat: Implement 'LoggerMiddleware' - Add LoggerMiddleware to log incoming requests method and URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { GithubModule } from './github/github.module'
+import { LoggerMiddleware } from './middlewares/logger.middleware'
 import { RepositoriesModule } from './repositories/repositories.module'
 import { ScoringModule } from './scoring/scoring.module'
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), RepositoriesModule, GithubModule, ScoringModule]
+    imports: [ConfigModule.forRoot({ isGlobal: true }), RepositoriesModule, GithubModule, ScoringModule],
+    providers: [LoggerMiddleware]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*')
+    }
+}
