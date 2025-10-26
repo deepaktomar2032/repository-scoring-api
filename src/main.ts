2bb38e6fa066c33a61bdb4b8e1f9c6f1aa58697e
deepaktomar2032 <deepaktomar2032@gmail.com>
2025-10-26 19:24:52 +0530

    Refactor: bootstrap function - to include CORS config. & improved error handling & exit codes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
+import { INestApplication, Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
 
+const logger: Logger = new Logger('Bootstrap Module')
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
-    await app.listen(process.env.PORT ?? 3000)
+    try {
+        const app: INestApplication = await NestFactory.create(AppModule)
+
+        app.enableCors({
+            origin: true,
+            methods: ['GET'],
+            allowedHeaders: ['Content-Type', 'Accept', 'Origin'],
+            credentials: false
+        })
+
+        const configService: ConfigService = app.get(ConfigService)
+
+        const PORT: number = configService.get<number>('PORT', 3000)
+
+        logger.log(`Server is up & running on Port: ${PORT}`)
+        logger.log(`API is ready to use: http://localhost:${PORT}`)
+
+        await app.listen(PORT)
+    } catch (error: unknown) {
+        logger.error('bootstrap: ', error)
+        process.exit(1)
+    }
 }
 
 bootstrap()
